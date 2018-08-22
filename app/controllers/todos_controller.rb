d14b5bc95cd2f2f699b9a42131b6a8262f70dc8c
Raphook <raphael69libert@gmail.com>
2018-08-22 10:31:54 +0200

    get /todos/:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,25 @@
 class TodosController < ApplicationController
+ before_action :fetch_todo, only: :show
+
  # GET /todos
   def index
     @todos = Todo.all
     json_response(@todos)
+  end
+
+  # GET /todos/:id
+  def show
+    json_response(@todo)
+  end
+
+  private
+
+  def todo_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def fetch_todo
+    @todo = Todo.find(params[:id])
   end
 end

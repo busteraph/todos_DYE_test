b76382883bd15524a3f5f4f547520daa2d0977d5
Raphook <raphael69libert@gmail.com>
2018-08-23 16:05:00 +0200

    get /todos

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :fetch_todo
+
+  # GET /todos/:todo_id/items
+  def index
+    json_response(@todo.items)
+  end
+
+  def fetch_todo
+    @todo = Todo.find(params[:todo_id])
+  end
 end
